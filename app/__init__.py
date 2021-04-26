ccee72c839e35a4095c02c06891c6680afff50c8
Dmitry <budilovdmi@gmail.com>
2021-04-27 11:41:42 +1200

    update uri for DATABASE_URL in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
